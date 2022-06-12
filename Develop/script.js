64d47a89018d99bfac7fc297251873a06010905a
Alex <lex.courtney18@gmail.com>
2022-06-12 08:58:35 -0500

    trying to remove commas between characters

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,8 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var character = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
-var password = [];
+//var password = [];
+//let text = password.join("");
 
 // 
 
@@ -125,9 +126,12 @@ function writePassword() {
     pw = character;
   }
 
+  var password = [];
+
   for (var i = 0; i < charLength; i++) {
     var pass = pw[Math.floor(Math.random() * pw.length)];
     password.push(pass);
+    let text = password.join("");
   }
   
   
